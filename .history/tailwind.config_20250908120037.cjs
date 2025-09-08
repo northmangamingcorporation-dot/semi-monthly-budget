3bd5eee13670e93942e90126de95f14d1f46fba1
northmangamingcorporation-dot <northmangamingcorporation@gmail.com>
2025-09-08 17:01:10 +0800

    FIX DIALOG

--- .history/tailwind.config_20250908120037.cjs
+++ .history/tailwind.config_20250908120037.cjs
@@ -5,11 +5,7 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-  extend: {
-    colors: {
-      background: "#ffffff", // change to white
-    }
-  }
-},
+    extend: {},
+  },
   plugins: [],
 }
